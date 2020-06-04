c65e70d7cc538fefee6591d6eff101b01590142b
Bennett Marsh <bmarsh@physics.ucsb.edu>
2020-06-03 23:29:52 -0500

    adding is_rivalry feature

--- scripts/add_display_names.py
+++ scripts/add_display_names.py
@@ -9,3 +9,6 @@ for tid in df.team_id:
     dns.append(d[tid])
 df.insert(1, "display_name", pd.Series(dns))
 print(df.head())
+
+with open('../data/teams.csv', 'w') as fid:
+    df.to_csv(fid, index=False)

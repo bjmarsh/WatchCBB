860fb156ff14594e7d650f2d3edb0a3bf7ce254d
Bennett Marsh <bmarsh9311@gmail.com>
2020-06-16 14:49:22 -0500

    creating class to handle postgresql :elephant: database reading/writing

--- watchcbb/sql.py
+++ watchcbb/sql.py
@@ -1,21 +1,53 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
+import sqlalchemy.exc
 
 import pandas as pd
 
 
-with open(os.path.join(os.path.dirname(__file__),"PSQL_CREDENTIALS.txt")) as fid:
-    cred = [x.strip() for x in fid.readlines()]
+class SQLEngine:
+    def __init__(self, dbname, credentials=None):
+        """ Open a connection to a postgresql database
+            
+            Parameters:
+            - dbname: is the name of the database
+            - credentials: either a tuple with (username,password), or a filename
+                           with username and password on first two lines.
+                           Defaults to {__file__}/PSQL_CREDENTIALS.txt
+        """
+        if credentials is None:
+            credentials = os.path.join(os.path.dirname(__file__),"PSQL_CREDENTIALS.txt")
 
-dbname = 'cbb'
-user = cred[0]
-pswd = cred[1]
+        if type(credentials) == str:
+            if not os.path.exists(credentials):
+                raise SQLEngine.SQLException(f"Credential {credentials} filename does not exist")
+            with open(credentials) as fid:
+                credentials = [x.strip() for x in fid.readlines()]
 
-## 'engine' is a connection to a database
-## Here, we're using postgres, but sqlalchemy can connect to other things too.
-engine = create_engine('postgresql://%s:%s@localhost/%s'%(user,pswd,dbname))
+        if type(credentials) in [tuple, list]:
+            if len(credentials) != 2 or type(credentials[0]) != str or type(credentials[1]) != str:
+                raise SQLEngine.SQLException("credentials must be a (user,password) tuple or filename")
+            username = credentials[0]
+            password = credentials[1]
+        else:
+            raise SQLEngine.SQLException("credentials must be a (user,password) tuple or filename")
 
-def df_from_query(query):
-    return pd.read_sql_query(query, engine)
+        self.engine = create_engine('postgresql://%s:%s@localhost/%s'%(credentials[0], credentials[1], dbname))
+        try:
+            self.engine.connect()
+        except sqlalchemy.exc.OperationalError:
+            raise SQLEngine.SQLException("invalid credentials or dbname")
+        
+    def df_from_query(self, query):
+        return pd.read_sql_query(query, self.engine)
 
+    def df_to_sql(self, df, table_name, if_exists='fail'):
+        df.to_sql(table_name, self.engine, index=False, if_exists=if_exists)
+
+    class SQLException(Exception):
+        pass
+
+
+if __name__=="__main__":
+    sql = SQLEngine('cbb')
+    print(sql.df_from_query(""" SELECT * FROM teams LIMIT 5 """))
